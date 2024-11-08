c64fb9ead9bcb12e889e2d1c264ee515faef4936
aobies59 <alvaroobiesgarcia@proton.me>
2024-11-08 09:54:44 +0100

    Finishing touches and run-all script

---
+++ scripts/run-all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sh scripts/EncuestasSatisfaccion.sh
+sh scripts/IncidenteSeguridad.sh
+sh scripts/Mantenimiento.sh
+sh scripts/RegistroClima.sh
+sh scripts/Usuario.sh
+sh scripts/Incidencias.sh
+sh scripts/Juegos.sh
+sh scripts/AreaRecreativa.sh
